f3f83b4f98728de0f28e0a9237e1db7a964ae7a5
libing <76359121@qq.com>
2014-06-08 19:31:47 +0800

    添加 backup commit update操作

--- tools/update.bat
+++ tools/update.bat
@@ -2,12 +2,4 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
-:: This is required with cygwin only.
-PATH=%~dp0;%PATH%
-call get_chromium_src.bat
-%~d0  
-cd %~dp0
-cd ..
-cd ..
-cd chromium
-gclient sync --force
+python "%~dp0\get_chromium_src.py" update
