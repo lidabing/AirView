f3eadf234d27c549b64b7b9e224a6f15ce1769ba
libing <76359121@qq.com>
2014-06-21 19:53:35 +0800

    调整编译选项 incremental_chrome_dll=1

---
+++ src/content/browser/web_contents/web_contents_view_aura_patch.cc
@@ -0,0 +1,5 @@
+// Copyright (c) 2014 The AirView Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/web_contents/web_contents_view_aura_patch.h"
--- tools/build_chromium_debug_x64.bat
+++ tools/build_chromium_debug_x64.bat
@@ -7,7 +7,4 @@ cd %~dp0
 cd ../..
 cd chromium
 cd src
-set GYP_MSVS_VERSION=2013
-set GYP_GENERATORS=msvs-ninja,ninja
-set GYP_DEFINES=component=shared_library incremental_chrome_dll=1 disable_nacl=1 fastbuild=1 chromium_win_pch=0
-python build/gyp_chromium --depth=.
+ninja -C out\Debug_x64 chrome.exe
