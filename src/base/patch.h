01fe1dd7a5f82e424da83099904cef9519e9c587
libing <76359121@qq.com>
2014-06-23 01:41:49 +0800

    add interface path method

--- src/base/patch.h
+++ src/base/patch.h
@@ -7,8 +7,7 @@
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 
-//给class打补丁的基础代码
-
+// PATCH CLASS
 #define X_PATCH_THIS(C)                     \
  public:                                    \
   C##Patch* patch_##C() { return &patch_; } \
@@ -24,12 +23,33 @@
   class C##Patch {             \
     C##Patch(C* that) : that_(that) {}
 
+//this have base interface's class patch
+#define X_START_CLASS_PATCH_(C, baseiface) \
+  class C;                                 \
+  class C##Patch : public baseiface {      \
+    C##Patch(C* that) : that_(that) {}
+
 #define X_END_CLASS_PATCH(C)          \
  private:                             \
   friend class C;                     \
   C* that_;                           \
   DISALLOW_COPY_AND_ASSIGN(C##Patch); \
   }                                   \
+  ;
+
+// PATCH INTERFACE
+#define X_INTERFACE_PATCH_NAME(iface) iface##Patch
+
+#define X_PATCH_INTERFACE(iface) \
+ public:                         \
+  virtual iface##Patch* patch_##iface() { return NULL; }
+
+
+#define X_START_INTERFACE_PATCH(export_type, iface) \
+  class export_type iface##Patch {                  \
+   public:
+#define X_END_INTERFACE_PATCH(iface) \
+  }                                  \
   ;
 
 #endif
