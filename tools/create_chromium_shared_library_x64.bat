d59793ab716c04a796c1961632dc8ae4e8ff04c1
libing <76359121@qq.com>
2014-06-22 13:17:46 +0800

    add create project opt "incremental_chrome_dll=1" for x86 and x64

--- tools/create_chromium_shared_library_x64.bat
+++ tools/create_chromium_shared_library_x64.bat
@@ -8,6 +8,7 @@ cd ../..
 cd chromium
 cd src
 set GYP_MSVS_VERSION=2013
-set GYP_GENERATORS=msvs-ninja,ninja
-set GYP_DEFINES=component=shared_library incremental_chrome_dll=1 disable_nacl=1 fastbuild=1 chromium_win_pch=0
+set GYP_GENERATORS=ninja
+set GYP_DEFINES=component=shared_library incremental_chrome_dll=1
+::disable_nacl=1 fastbuild=1 chromium_win_pch=1
 python build/gyp_chromium --depth=. -Dtarget_arch=x64 
