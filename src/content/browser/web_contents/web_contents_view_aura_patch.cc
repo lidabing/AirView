157a4de56abaa2dc9bbc09efacf9f30ed7b7b6f3
lidabing <76359121@qq.com>
2014-06-24 17:17:49 +0800

    解决超级拖拽和HTML5拖拽之间冲突问题，HTML5的拖拽优先

--- src/content/browser/web_contents/web_contents_view_aura_patch.cc
+++ src/content/browser/web_contents/web_contents_view_aura_patch.cc
@@ -6,7 +6,33 @@
 #include "content/browser/web_contents/web_contents_view_aura.h"
 #include "content/public/browser/x_web_drag_drop_delegate.h"
 #include "content/browser/web_contents/web_contents_impl.h"
+#include "ui/events/event.h"
+#include "ui/events/event_utils.h"
+#include "ui/base/dragdrop/drop_target_event.h"
 
+namespace {
+	int ConvertFromWeb(blink::WebDragOperationsMask ops) {
+		int drag_op = ui::DragDropTypes::DRAG_NONE;
+		if (ops & blink::WebDragOperationCopy)
+			drag_op |= ui::DragDropTypes::DRAG_COPY;
+		if (ops & blink::WebDragOperationMove)
+			drag_op |= ui::DragDropTypes::DRAG_MOVE;
+		if (ops & blink::WebDragOperationLink)
+			drag_op |= ui::DragDropTypes::DRAG_LINK;
+		return drag_op;
+	}
+
+	blink::WebDragOperationsMask ConvertToWeb(int drag_op) {
+		int web_drag_op = blink::WebDragOperationNone;
+		if (drag_op & ui::DragDropTypes::DRAG_COPY)
+			web_drag_op |= blink::WebDragOperationCopy;
+		if (drag_op & ui::DragDropTypes::DRAG_MOVE)
+			web_drag_op |= blink::WebDragOperationMove;
+		if (drag_op & ui::DragDropTypes::DRAG_LINK)
+			web_drag_op |= blink::WebDragOperationLink;
+		return (blink::WebDragOperationsMask) web_drag_op;
+	}
+}
 namespace content {
 void X_CLASS_PATCH_NAME(WebContentsViewAura)::Constructor(
     XWebDragDropDelegate* dest) {
@@ -18,17 +44,20 @@ void X_CLASS_PATCH_NAME(WebContentsViewAura)::OnDragEntered(
   web_drag_dest_delegate_->OnDragEntered(event);
 }
 int X_CLASS_PATCH_NAME(WebContentsViewAura)::OnDragUpdated(
-    const ui::DropTargetEvent& event) {
-  web_drag_dest_delegate_->OnDragUpdated(event);
-  that_->current_drag_op_ = blink::WebDragOperationLink;
+	const ui::DropTargetEvent& event) {
+		web_drag_dest_delegate_->OnDragUpdated(event);
+		if (that_->current_drag_op_ == blink::WebDragOperationNone)
+			that_->current_drag_op_ = blink::WebDragOperationLink;
+		
   return 1;
 }
 void X_CLASS_PATCH_NAME(WebContentsViewAura)::OnDragExited() {
-  web_drag_dest_delegate_->OnDragExited();
+	web_drag_dest_delegate_->OnDragExited();
 }
 int X_CLASS_PATCH_NAME(WebContentsViewAura)::OnPerformDrop(
     const ui::DropTargetEvent& event) {
-  web_drag_dest_delegate_->OnPerformDrop(event);
+	if (that_->current_drag_op_ == blink::WebDragOperationNone)
+	  web_drag_dest_delegate_->OnPerformDrop(event);
   return 1;
 }
 }
