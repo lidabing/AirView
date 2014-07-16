fd88ef6e2653783858811bc5ae4c900d5c997a20
lidabing <76359121@qq.com>
2014-07-16 08:41:55 +0800

    fix create_chromium error

--- tools/create_chromium.bat
+++ tools/create_chromium.bat
@@ -8,6 +8,5 @@ cd ../..
 cd chromium
 cd src
 set GYP_MSVS_VERSION=2013
-set GYP_GENERATORS=msvs-ninja,ninja incremental_chrome_dll=1 disable_nacl=1
-python build/gyp_chromium --depth=.
-
+set GYP_GENERATORS=msvs-ninja,ninja
+python build/gyp_chromium --depth=. 
