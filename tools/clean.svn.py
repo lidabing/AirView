fb1ac2ca0e275661cab8ef2837172ed4d52bab96
lidabing <76359121@qq.com>
2014-08-07 10:38:51 +0800

    add clean.svn

---
+++ tools/clean.svn.py
@@ -0,0 +1,35 @@
+import os, stat;
+
+root_dir = 'C:/chromium1/chromium/';
+
+def walk(path):
+     for item in os.listdir(path):
+         subpath = os.path.join(path, item);
+         mode = os.stat(subpath)[stat.ST_MODE];
+         
+         if stat.S_ISDIR(mode):
+             if item==".svn":
+                 print "Clean %s ..." % subpath;
+                 print "%d deleted!" % purge(subpath);
+             else:
+                 walk(subpath);
+
+def purge(path):
+     count = 0;
+     for item in os.listdir(path):
+         subpath = os.path.join(path, item);
+         mode = os.stat(subpath)[stat.ST_MODE];
+         if stat.S_ISDIR(mode):
+             count += purge(subpath);
+         else:
+             os.chmod(subpath, stat.S_IREAD|stat.S_IWRITE);
+             os.unlink(subpath);
+             count += 1;
+     os.rmdir(path);
+     count += 1;
+     return count;
+                      
+     
+
+if __name__=='__main__':
+     walk(root_dir);
