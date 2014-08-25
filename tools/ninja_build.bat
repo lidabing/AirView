d829f2bdf2b9c01e384dadd5b304ebe74137dffd
lidabing <76359121@qq.com>
2014-08-25 17:17:24 +0800

    fix python break bat process

--- tools/ninja_build.bat
+++ tools/ninja_build.bat
@@ -4,7 +4,7 @@
 
 ::新的创建工程功能，老的创建和生成方式文件太多，使用现在的去做优化
 
-@echo off
+::@echo off
 
 cd %~dp0
 
@@ -46,7 +46,8 @@ goto next-arg
 :args-done
 if "%config%"=="Debug" set output_dir=Debug
 if "%config%"=="Release" set output_dir=Release
-if "%target_arch%"=="x64" set target_arch_mode=target_arch=x64 set output_dir=%config%_x64
+if "%target_arch%"=="x64" set target_arch_mode=target_arch=x64 
+if "%target_arch%"=="x64" set output_dir=%config%_x64
 if "%link_mode%"=="shared" set component_mode=component=shared_library;
 goto install-environment
 
@@ -63,18 +64,14 @@ goto create-project
 echo create-project
 set GYP_GENERATORS=msvs-ninja,ninja
 set GYP_DEFINES=%component_mode% %target_arch_mode% branding=Chrome disable_nacl=1  disable_pnacl=1 disable_nacl_untrusted=1 fastbuild=1 remoting=0  enable_plugin_installation=0 enable_google_now=0  chromium_win_pch=1 enable_webrtc=1 tracing_like_official_build=1 buildtype=official ui_compositor_image_transport=0 enable_printing=1 enable_plugins=1   use_openmax_dl_fft=0 enable_eglimage=0   clang_use_chrome_plugins=0 remove_webcore_debug_symbols=1 use_xi2_mt=0 enable_mdns=0  skia_support_gpu=0 cld_version=1
-SETLOCAL
-python build/gyp_chromium --depth=. -D msbuild_toolset=v110_xp
+call python build/gyp_chromium --depth=. -D msbuild_toolset=v110_xp
 echo Project files generated.
-ENDLOCAL
-
 goto build
 
 :build
-echo build_project
-ninja -C out\%debug_arg% mini_installer.exe
-
-
+echo %output_dir%
+ninja -C out\%output_dir% mini_installer.exe
+goto exit
 
 :help
 echo ninja_build.bat [release/debug] [ia32/x64] [static/shared]
