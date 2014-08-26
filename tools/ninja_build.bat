f2d04ce1e7039cea31aed44c4492e3c319bb231b
lidabing <bzero1982@gmail.com>
2014-08-26 22:09:33 +0800

    use default cld

--- tools/ninja_build.bat
+++ tools/ninja_build.bat
@@ -65,7 +65,7 @@ goto create-project
 :create-project
 echo create-project
 set GYP_GENERATORS=msvs-ninja,ninja
-set GYP_DEFINES=%component_mode% %target_arch_mode% branding=Chrome disable_nacl=1  disable_pnacl=1 disable_nacl_untrusted=1 fastbuild=1 remoting=0  enable_plugin_installation=0 enable_google_now=0  chromium_win_pch=1 enable_webrtc=1 tracing_like_official_build=1 buildtype=official ui_compositor_image_transport=0 enable_printing=1 enable_plugins=1   use_openmax_dl_fft=0 enable_eglimage=0   clang_use_chrome_plugins=0 remove_webcore_debug_symbols=1 use_xi2_mt=0 enable_mdns=0  skia_support_gpu=0 cld_version=1
+set GYP_DEFINES=%component_mode% %target_arch_mode% branding=Chrome disable_nacl=1  disable_pnacl=1 disable_nacl_untrusted=1 fastbuild=1 remoting=0  enable_plugin_installation=0 enable_google_now=0  chromium_win_pch=1 enable_webrtc=1 tracing_like_official_build=1 buildtype=official ui_compositor_image_transport=0 enable_printing=1 enable_plugins=1   use_openmax_dl_fft=0 enable_eglimage=0   clang_use_chrome_plugins=0 remove_webcore_debug_symbols=1 use_xi2_mt=0 enable_mdns=0  skia_support_gpu=0
 call python build/gyp_chromium --depth=. -D msbuild_toolset=v110_xp
 echo Project files generated.
 goto build
